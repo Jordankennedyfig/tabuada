b06062e027117d1fcf295f5901729bc07049d0b6
Jordan Figueiredo <jordankennedyfigueiredo@gmail.com>
2024-05-03 20:00:49 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,24 @@
+print ('===========Seja Bem Vindo a Tabuada Python========== '
+       '\nAqui Você Vai Aprender Matemática e Programação num só Lugar :) '
+       '\nEsta Pronto ?')
+nome = input('Antes de tudo, me diga o seu nome: ')
+print('Olá {}, tudo bem? '.format(nome))
+n = int(input('Digite um número que vou te mostrar a tabuada dele agora mesmo: '))
+
+print('=========================')
+print('{} X 1 = {}'.format(n, n * 1))
+print('{} X 2 = {}'.format(n, n * 2))
+print('{} X 3 = {}'.format(n, n * 3))
+print('{} X 4 = {}'.format(n, n * 4))
+print('{} X 5 = {}'.format(n, n * 5))
+print('{} X 6 = {}'.format(n, n * 6))
+print('{} X 7 = {}'.format(n, n * 7))
+print('{} X 8 = {}'.format(n, n * 8))
+print('{} X 9 = {}'.format(n, n * 9))
+print('{} X 10 = {}'.format(n, n * 10))
+print('=========================')
+
+print('Muito Bem!!! '
+      '\nQuando esquercer das aulas de matemática de Tião, só correr aqui que eu te ajudo.'
+      '\nSó não conta pra ninguém :( '
+      '\nAté Logo....')
